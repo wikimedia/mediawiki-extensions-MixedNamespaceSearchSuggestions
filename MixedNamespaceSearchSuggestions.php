7ff457919ebcbe10f7f5ba2abf97f30130cbe3b8
Niklas Laxström <niklas.laxstrom@gmail.com>
2017-11-08 10:23:47 +0100

    Convert to extension registration
    Change-Id: Ie0354ec44649d0225b151c0f2069fb3775ec8fc7

--- MixedNamespaceSearchSuggestions.php
+++ MixedNamespaceSearchSuggestions.php
@@ -1,28 +1,3 @@
 <?php
-/**
- * @author Niklas Laxström
- * @license MIT
- */
 
-if ( !defined( 'MEDIAWIKI' ) ) {
-	die();
-}
-
-$wgExtensionCredits['other'][] = [
-	'path' => __FILE__,
-	'name' => 'MixedNamespaceSearchSuggestions',
-	'version' => '2015-12-04',
-	'author' => 'Niklas Laxström',
-	'descriptionmsg' => 'mnss-desc',
-	'url' => 'https://www.mediawiki.org/wiki/Extension:MixedNamespaceSearchSuggestions',
-	'license-name' => 'MIT',
-];
-
-$dir = __DIR__;
-require_once "$dir/Resources.php";
-
-$wgMessagesDirs['MNSS'] = "$dir/i18n";
-
-$wgHooks['BeforePageDisplay'][] = function ( OutputPage $out ) {
-	$out->addModules( 'ext.mnss.search' );
-};
+wfLoadExtension( 'MixedNamespaceSearchSuggestions' );
